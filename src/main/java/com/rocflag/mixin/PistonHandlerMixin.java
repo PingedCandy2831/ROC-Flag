03d6d57ea05f0e4c5aa7ec1a05231e6755398a98
PingedCandy2831 <zhoujingjun2004821@gmail.com>
2024-04-29 14:34:47 +0800

    ROC-Flag 1.0.1_1.20.1_build.10

---
+++ src/main/java/com/rocflag/mixin/PistonHandlerMixin.java
@@ -0,0 +1,77 @@
+package com.rocflag.mixin;
+
+import com.rocflag.block.ModBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.piston.PistonHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PistonHandler.class)
+public class PistonHandlerMixin {
+
+    @Inject(at = @At("HEAD"), method = "isBlockSticky", cancellable = true)
+    private static void slimeology$isBlockStickyMixin(BlockState state, CallbackInfoReturnable<Boolean> cir) {
+        if (state.getBlock() == ModBlocks.BOTTOM_LEFT_BLOCK) {
+            cir.setReturnValue(true);
+        }
+        if (state.getBlock() == ModBlocks.BOTTOM_MIDDLE_BLOCK) {
+            cir.setReturnValue(true);
+        }
+        if (state.getBlock() == ModBlocks.BOTTOM_RIGHT_BLOCK) {
+            cir.setReturnValue(true);
+        }
+        if (state.getBlock() == ModBlocks.TOP_LEFT_BLOCK) {
+            cir.setReturnValue(true);
+        }
+        if (state.getBlock() == ModBlocks.TOP_MIDDLE_BLOCK) {
+            cir.setReturnValue(true);
+        }
+        if (state.getBlock() == ModBlocks.TOP_RIGHT_BLOCK) {
+            cir.setReturnValue(true);
+        }
+        if (state.getBlock() == ModBlocks.ROC_RED_BLOCK) {
+            cir.setReturnValue(true);
+        }
+        if (state.getBlock() == ModBlocks.ROC_BLUE_BLOCK) {
+            cir.setReturnValue(true);
+        }
+        if (state.getBlock() == ModBlocks.ROC_FLAG_BLOCK) {
+            cir.setReturnValue(true);
+        }
+    }
+
+    @Inject(at = @At("HEAD"), method = "isAdjacentBlockStuck", cancellable = true)
+    private static void slimeology$isAdjacentBlockStuckMixin(BlockState state, BlockState adjacentState, CallbackInfoReturnable<Boolean> cir) {
+        if (state.getBlock() == ModBlocks.BOTTOM_LEFT_BLOCK && adjacentState.getBlock() == ModBlocks.BOTTOM_LEFT_BLOCK) {
+            cir.setReturnValue(true);
+        }
+        if (state.getBlock() == ModBlocks.BOTTOM_MIDDLE_BLOCK && adjacentState.getBlock() == ModBlocks.BOTTOM_MIDDLE_BLOCK) {
+            cir.setReturnValue(true);
+        }
+        if (state.getBlock() == ModBlocks.BOTTOM_RIGHT_BLOCK && adjacentState.getBlock() == ModBlocks.BOTTOM_RIGHT_BLOCK) {
+            cir.setReturnValue(true);
+        }
+        if (state.getBlock() == ModBlocks.TOP_LEFT_BLOCK && adjacentState.getBlock() == ModBlocks.TOP_LEFT_BLOCK) {
+            cir.setReturnValue(true);
+        }
+        if (state.getBlock() == ModBlocks.TOP_MIDDLE_BLOCK && adjacentState.getBlock() == ModBlocks.TOP_MIDDLE_BLOCK) {
+            cir.setReturnValue(true);
+        }
+        if (state.getBlock() == ModBlocks.TOP_RIGHT_BLOCK && adjacentState.getBlock() == ModBlocks.TOP_RIGHT_BLOCK) {
+            cir.setReturnValue(true);
+        }
+        if (state.getBlock() == ModBlocks.ROC_RED_BLOCK && adjacentState.getBlock() == ModBlocks.ROC_RED_BLOCK) {
+            cir.setReturnValue(true);
+        }
+        if (state.getBlock() == ModBlocks.ROC_BLUE_BLOCK && adjacentState.getBlock() == ModBlocks.ROC_BLUE_BLOCK) {
+            cir.setReturnValue(true);
+        }
+        if (state.getBlock() == ModBlocks.ROC_FLAG_BLOCK && adjacentState.getBlock() == ModBlocks.ROC_FLAG_BLOCK){
+            cir.setReturnValue(true);
+        }
+    }
+}
